fc6a3b43d8572f756a36416852d7d36de5459662
Ivan Komarov <Ivan.Komarov@dfyz.info>
2016-04-11 02:45:06 +0500

    Initial Commit

---
+++ RouterOSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RouterOSX
+//
+//  Created by Ivan Komarov on 11/04/16.
+//  Copyright © 2016 Ivan Komarov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
