cbc323707c16b98225c23909b8225389990f52e2
Ivan Komarov <Ivan.Komarov@dfyz.info>
2016-04-15 01:25:55 +0300

    fb swift

---
+++ RouterOSX/FlatBuffersSwift/FlatBuffers.swift
@@ -0,0 +1,41 @@
+//
+//  FlatBuffers.swift
+//  SwiftFlatBuffers
+//
+//  Created by Maxim Zaks on 21.11.15.
+//  Copyright © 2015 maxim.zaks. All rights reserved.
+//
+
+public typealias Offset = Int32
+
+public protocol Scalar : Equatable {}
+
+extension Scalar {
+    var littleEndian : Self {
+        switch self {
+        case let v as Int16 : return v.littleEndian as! Self
+        case let v as UInt16 : return v.littleEndian as! Self
+        case let v as Int32 : return v.littleEndian as! Self
+        case let v as UInt32 : return v.littleEndian as! Self
+        case let v as Int64 : return v.littleEndian as! Self
+        case let v as UInt64 : return v.littleEndian as! Self
+        case let v as Int : return v.littleEndian as! Self
+        case let v as UInt : return v.littleEndian as! Self
+        default : return self
+        }
+    }
+}
+
+extension Bool : Scalar {}
+extension Int8 : Scalar {}
+extension UInt8 : Scalar {}
+extension Int16 : Scalar {}
+extension UInt16 : Scalar {}
+extension Int32 : Scalar {}
+extension UInt32 : Scalar {}
+extension Int64 : Scalar {}
+extension UInt64 : Scalar {}
+extension Int : Scalar {}
+extension UInt : Scalar {}
+extension Float32 : Scalar {}
+extension Float64 : Scalar {}
