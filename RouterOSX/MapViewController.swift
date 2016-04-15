f687c1df83a97491c6922dd097fdf418002445aa
Ivan Komarov <Ivan.Komarov@dfyz.info>
2016-04-15 05:01:41 +0300

    nils

--- RouterOSX/MapViewController.swift
+++ RouterOSX/MapViewController.swift
@@ -9,10 +9,20 @@ class MapViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let mapArea = stage.mapArea {
+        if let mapArea = stage?.mapArea {
             let center = CLLocationCoordinate2D(latitude: mapArea.centerLat, longitude: mapArea.centerLon)
             let span = MKCoordinateSpan(latitudeDelta: mapArea.height, longitudeDelta: mapArea.width)
             mapView.region = MKCoordinateRegion(center: center, span: span)
         }
+    }
+
+    override func viewWillAppear() {
+        super.viewWillAppear()
+
+        if stage == nil {
+            return
+        }
+
+        view.window?.title = "\(stage.competitionName) — \(stage.stageNumber)"
     }
 }
